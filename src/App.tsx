a3e0ebaf8331bb6a5dd74a23ec0f6e3b2848f292
XHULIA PRISHKA <xh.prishka@gmail.com>
2022-08-18 19:00:15 +0200

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,90 +2,120 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [items, setItems]=useState([
-    ["🦄", "🦄", "🦄"],
-    ["🦄", "🦄", "🦄"],
-    ["🦄", "🦄", "🦄"],
-  ])
-
+  const [board, setBoard]=useState(
+    Array(9).fill(null)
+  )
   const [count, setCounter]=useState(0)
-  
+  const [winner, setWinner]=useState('')
+  const [player, setPlayer]=useState(0)
+
   function getLetter(){
     let letter=''
     if(count%2===0){
+      setPlayer(2)
      return letter="X"
     }
     else{
+      setPlayer(1)
       return letter="O"
     }
   }
   
+  // function getWinner(){
+  //   for(let n=0; n<3; n++){
+  //     for(let m=0; m<3; m++){
+  //   if((items[n][0]===items[n][1] && items[n][1]===items[0][2]) 
+  //   || (items[0][m]===items[1][m] && items[2][m]===items[0][2])
+  //   || (items[0][0]===items[1][1] && items[1][1]===items[2][2])
+  //   || (items[2][0]===items[1][1] && items[1][1]===items[0][2])
+  //   )
+    
+  //   console.log("we have a winner")
+    
+  //   }
 
-const copyState=structuredClone(items)
+  // }}
+
+  // getWinner()
+
+const copyState=structuredClone(board)
   return (
     <div className="App">
       <header className="App-header">
         <h1>TIC-TAC-TOE GAME 🦄</h1>
-        <h2> Player 1: X </h2>
-        <h2> Player 2: O </h2>
 
-        <div className="game-container">
+        {/* <div className="game-container">
+          {board.map(item=>(
+
+             <span
+              onClick={()=>{
+              setCounter(count+1)
+              console.log(item, board.indexOf(item))
+              copyState[board.indexOf(item)]=getLetter()
+              setBoard(copyState)
+            }}>{item}</span>
+          ))}
+        </div> */}
+        {player===2 ? <h3>Player O turn</h3>: <h3>Player X turn</h3>}
+         <div className="game-container">
           
           <span onClick={()=>{
             setCounter(count+1)
-            copyState[0][0]=getLetter()
-            setItems(copyState)
-          }}>{items[0][0]}</span>
+            copyState[0]=getLetter()
+            setBoard(copyState)
+          }}>{board[0]}</span>
 
             <span onClick={()=>{
             setCounter(count+1)
-            copyState[0][1]=getLetter()
-            setItems(copyState)
-          }}>{items[0][1]}</span>
+            copyState[1]=getLetter()
+            setBoard(copyState)
+          }}>{board[1]}</span>
 
             <span onClick={()=>{
             setCounter(count+1)
-            copyState[0][2]=getLetter()
-            setItems(copyState)
-          }}>{items[0][2]}</span>
+            copyState[2]=getLetter()
+            setBoard(copyState)
+          }}>{board[2]}</span>
 
             <span onClick={()=>{
             setCounter(count+1)
-            copyState[1][0]=getLetter()
-            setItems(copyState)
-          }}>{items[1][0]}</span>
+            copyState[3]=getLetter()
+            setBoard(copyState)
+          }}>{board[3]}</span>
 
             <span onClick={()=>{
             setCounter(count+1)
-            copyState[1][1]=getLetter()
-            setItems(copyState)
-          }}>{items[1][1]}</span>
+            copyState[4]=getLetter()
+            setBoard(copyState)
+          }}>{board[4]}</span>
 
             <span onClick={()=>{
             setCounter(count+1)
-            copyState[1][2]=getLetter()
-            setItems(copyState)
-          }}>{items[1][2]}</span>
+            copyState[5]=getLetter()
+            setBoard(copyState)
+          }}>{board[5]}</span>
 
             <span onClick={()=>{
             setCounter(count+1)
-            copyState[2][0]=getLetter()
-            setItems(copyState)
-          }}>{items[2][0]}</span>
+            copyState[6]=getLetter()
+            setBoard(copyState)
+          }}>{board[6]}</span>
 
             <span onClick={()=>{
             setCounter(count+1)
-            copyState[2][1]=getLetter()
-            setItems(copyState)
-          }}>{items[2][1]}</span>
+            copyState[7]=getLetter()
+            setBoard(copyState)
+          }}>{board[7]}</span>
 
             <span onClick={()=>{
             setCounter(count+1)
-            copyState[2][2]=getLetter()
-            setItems(copyState)
-          }}>{items[2][2]}</span>
+            copyState[8]=getLetter()
+            setBoard(copyState)
+          }}>{board[8]}</span>
           
         </div>
+
+  
       </header>
     </div>
   );
