4fa043202186b0852cde4a1f004affe6ae8fea3e
XHULIA PRISHKA <xh.prishka@gmail.com>
2022-08-18 20:12:44 +0200

    final commit for now

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,25 @@ import "./App.css";
 
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
-  const [winner, setWinner] = useState("");
   const [player, setPlayer] = useState("O");
+
+let boardIndexes=[
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+]
+
+for(let item=0; item<boardIndexes.length; item++){
+  const[item1, item2, item3]=boardIndexes[item];
+  if(board[item1]&& board[item1]===board[item2] && board[item2]=== board[item3]){
+alert(`${(board[item1])} just won!!`)
+  }
+}
 
   function play(index) {
     let newPlayer = player === "X" ? "O" : "X";
